c382ed29c91d91b1ec0b6f93c1fb03cf31e3097b
Liam Cabral <liamcabral88@gmail.com>
2021-03-26 20:05:12 -0300

    comments

--- main.cpp
+++ main.cpp
@@ -3,25 +3,36 @@
 
 using namespace std;
 
+// Função inicial da aplicação
 int main() {
+    // Opção de qual conversão o usuario deseja
     int opcao;
+    // Valor que o usuario deseja converter
     int valor;
 
+    // Apresentação e captura de qual a conversão desejada
     cout << "Seja bem vindo a conversao de bases do Liam" << endl;
     cout << "\n1 - Decimal para hexadecimal" << endl;
     cout << "2 - Hexadecimal para decimal \n" << endl;
     cout << "Digite a opcao desejada: " << endl;
     cin >> opcao;
 
+    // Case 1 conversão de decimal para hexadecimal
     if(opcao == 1) {
         cout << "\nDigite o decimal: " << endl;
         scanf("%d", &valor);
+        // Retorna o decimal convertido para hexadecimal
         printf("\n%d para hexadecimal eh: %x\n", valor, valor);
-    } else if(opcao == 2) {
+    } 
+    // Case 2 conversão de hexadecimal para decimal
+    else if(opcao == 2) {
         cout << "\nDigite o hexadecimal: " << endl;
         scanf("%x", &valor);
+        // Retorna hexadecimal convertido para decimal
         printf("\n%x para decimal eh: %d\n", valor, valor);
-    } else {
+    } 
+    // Case opção informada seja invalida
+    else {
         printf("\nOpcao informada invalida.");
     }
 
